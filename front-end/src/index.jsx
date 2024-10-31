875d636a0d0f4207c1b9374abc35c348807360a4
Lincoln Junior <99145408+lincolnjpg@users.noreply.github.com>
2024-10-31 00:41:42 -0300

    initial commit (add wip front-end)

---
+++ front-end/src/index.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './components/ErrorPage';
+import Home from './components/Home';
+import Movies from './components/Movies';
+import Genres from './components/Genres';
+import EditMovie from './components/EditMovie';
+import ManageCatalog from './components/ManageCatalog';
+import GraphQL from './components/GraphQL';
+import Login from './components/Login';
+import Movie from './components/Movie';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/movies",
+        element: <Movies />,
+      },
+      {
+        path: "/movies/:id",
+        element: <Movie />,
+      },
+      {
+        path: "/genres",
+        element: <Genres />,
+      },
+      {
+        path: "/admin/movie/0",
+        element: <EditMovie />,
+      },
+      {
+        path: "/manage-catalog",
+        element: <ManageCatalog />,
+      },
+      {
+        path: "/graphql",
+        element: <GraphQL />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ]
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
