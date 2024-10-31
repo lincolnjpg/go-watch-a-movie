f28f1fbd1ac55fe37ca8037be7872b85ddc7fc8e
Lincoln Junior <99145408+lincolnjpg@users.noreply.github.com>
2024-10-31 03:52:21 -0300

    add call to fetch fake movies from front-end

--- back-end/cmd/api/routes.go
+++ back-end/cmd/api/routes.go
@@ -10,6 +10,7 @@ import (
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
+	mux.Use(app.enableCors)
 	mux.Get("/home", app.Home)
 	mux.Get("/movies", app.AllMovies)
 
